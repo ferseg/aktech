3e7dfdb887061caf17a459e4fdb7bc75e72d21e7
Roy Cordero <roy.cordero@akurey.com>
2022-09-22 15:21:31 -0600

    Refactoring repositories to support both hibernate and SPs

--- codebase/java-micronaut/src/student/src/main/java/com/akurey/repositories/StudentRepository.java
+++ codebase/java-micronaut/src/student/src/main/java/com/akurey/repositories/StudentRepository.java
@@ -1,10 +1,33 @@
 package com.akurey.repositories;
 
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import com.akurey.common.exceptions.CustomException;
+import com.akurey.common.repositories.BaseRepository;
 import com.akurey.repositories.entities.Student;
+import com.akurey.repositories.entities.TestParams;
+import com.akurey.repositories.entities.TestResult;
 
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.repository.CrudRepository;
 
 @Repository
-public interface StudentRepository extends CrudRepository<Student, Long> {
+public abstract class StudentRepository extends BaseRepository implements CrudRepository<Student, Long> {
+
+  private final EntityManager entityManager;
+
+  public StudentRepository(EntityManager entityManager) {
+    this.entityManager = entityManager;
+  }
+
+  @Override
+  protected EntityManager getEntityManager() {
+    return entityManager;
+  }
+
+  @Transactional
+  public TestResult test(TestParams params) throws CustomException {
+    return getSingleResult(params, TestResult.class);
+  }
 }
